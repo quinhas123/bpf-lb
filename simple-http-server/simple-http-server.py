28cd505cf53983f6022cd45b86dbc6135f9e84eb
quinhas123 <lucasmschirmbeck@gmail.com>
2025-11-08 18:52:41 -0300

    feat: uv init of simple python http server

---
+++ simple-http-server/simple-http-server.py
@@ -0,0 +1,14 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "dotenv",
+# ]
+# ///
+
+
+def main() -> None:
+    print("Hello from simple-http-server.py!")
+
+
+if __name__ == "__main__":
+    main()
