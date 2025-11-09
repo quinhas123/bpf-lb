1482d8a078143572e340315525a89f9fbaf97137
quinhas123 <lucasmschirmbeck@gmail.com>
2025-11-09 18:18:17 -0300

    feat: scripts for docker container orchestration

---
+++ scripts/start-containers.sh
@@ -0,0 +1,2 @@
+sudo docker compose up server-1 -d
+sudo docker compose up server-2 -d
---
+++ scripts/stop-rm-containers.sh
@@ -0,0 +1,4 @@
+sudo docker stop server-1
+sudo docker rm server-1
+sudo docker stop server-2
+sudo docker rm server-2
