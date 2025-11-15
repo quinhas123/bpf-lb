0506acf0ddfa328fbc7c78236f34503a05c61453
quinhas123 <lucasmschirmbeck@gmail.com>
2025-11-15 12:12:24 -0300

    fix: use nginx containers for now

--- scripts/start-containers.sh
+++ scripts/start-containers.sh
@@ -1,2 +1,6 @@
-sudo docker compose up server-1 -d
-sudo docker compose up server-2 -d
+docker run -d --rm --name backend-A --mac-address 02:42:ac:11:00:02 -h backend-A --env TERM=xterm-color nginxdemos/hello:plain-text
+docker run -d --rm --name backend-B --mac-address 02:42:ac:11:00:03 -h backend-B --env TERM=xterm-color nginxdemos/hello:plain-text
+#sudo docker run -d --rm -it --name client --mac-address 02:42:ac:11:00:04 -h client --env TERM=xterm-color client
+#sudo docker run --rm -it --privileged -h lb --name lb --mac-address 02:42:ac:11:00:05 --env TERM=xterm-color lb
+
+#docker run -d --name server-2 -h server-2 -p 81:8000 http-server uv run --script simple-http-server.py -i 2
--- scripts/stop-rm-containers.sh
+++ scripts/stop-rm-containers.sh
@@ -1,4 +1,6 @@
-sudo docker stop server-1
-sudo docker rm server-1
-sudo docker stop server-2
-sudo docker rm server-2
+# TODO: add --rm to docker compose up
+sudo docker stop backend-A backend-B client lb
+#sudo docker stop server-1
+#sudo docker rm server-1
+#sudo docker stop server-2
+#sudo docker rm server-2
