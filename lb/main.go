cba09c71213e02dd67189909969d26d8aed96751
quinhas123 <lucasmschirmbeck@gmail.com>
2025-11-15 12:09:33 -0300

    rm: packet counting loop

--- lb/main.go
+++ lb/main.go
@@ -1,66 +1,55 @@
 package main
 
 import (
-    "log"
-    "net"
-    "os"
-    "os/signal"
-    "time"
+	"log"
+	"net"
+	"os"
+	"os/signal"
 
-    "github.com/cilium/ebpf/link"
-    "github.com/cilium/ebpf/rlimit"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
 )
 
-//go generate && go build && sudo ./ebpf-test
+// go generate && go build && sudo ./ebpf-test
 func main() {
-    // Remove resource limits for kernels <5.11.
-    if err := rlimit.RemoveMemlock(); err != nil { 
-        log.Fatal("Removing memlock:", err)
-    }
+	// Remove resource limits for kernels <5.11.
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal("Removing memlock:", err)
+	}
 
-    // Load the compiled eBPF ELF and load it into the kernel.
-    var objs counterObjects 
-    if err := loadCounterObjects(&objs, nil); err != nil {
-        log.Fatal("Loading eBPF objects:", err)
-    }
-    defer objs.Close() 
+	// Load the compiled eBPF ELF and load it into the kernel.
+	var objs counterObjects
+	if err := loadCounterObjects(&objs, nil); err != nil {
+		log.Fatal("Loading eBPF objects:", err)
+	}
+	defer objs.Close()
 
-    ifname := "enp0s3" // Change this to an interface on your machine.
-    iface, err := net.InterfaceByName(ifname)
-    if err != nil {
-        log.Fatalf("Getting interface %s: %s", ifname, err)
-    }
+	ifname := "eth0" // Change this to an interface on your machine.
+	iface, err := net.InterfaceByName(ifname)
+	if err != nil {
+		log.Fatalf("Getting interface %s: %s", ifname, err)
+	}
 
-    // Attach count_packets to the network interface.
-    link, err := link.AttachXDP(link.XDPOptions{ 
-        Program:   objs.CountPackets,
-        Interface: iface.Index,
-    })
-    if err != nil {
-        log.Fatal("Attaching XDP:", err)
-    }
-    defer link.Close() 
+	// Attach count_packets to the network interface.
+	link, err := link.AttachXDP(link.XDPOptions{
+		Program:   objs.CountPackets,
+		Interface: iface.Index,
+		Flags:     link.XDPGenericMode, // matches bpftool
 
-    log.Printf("Counting incoming packets on %s..", ifname)
+	})
+	if err != nil {
+		log.Fatal("Attaching XDP:", err)
+	}
+	defer link.Close()
 
-    // Periodically fetch the packet counter from PktCount,
-    // exit the program when interrupted.
-    tick := time.Tick(time.Second)
-    stop := make(chan os.Signal, 5)
-    signal.Notify(stop, os.Interrupt)
-    for {
-        select {
-        case <-tick:
-            var count uint64
-            err := objs.PktCount.Lookup(uint32(0), &count) 
-            if err != nil {
-                log.Fatal("Map lookup:", err)
-            }
-            log.Printf("Received %d packets", count)
-        case <-stop:
-            log.Print("Received signal, exiting..")
-            return
-        }
-    }
+	log.Printf("Counting incoming packets on %s..", ifname)
+
+	// Periodically fetch the packet counter from PktCount,
+	// exit the program when interrupted.
+	stop := make(chan os.Signal, 5)
+	signal.Notify(stop, os.Interrupt)
+	for range stop {
+		log.Print("Received signal, exiting..")
+		return
+	}
 }
-
